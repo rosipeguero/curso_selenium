220919c52090c96b03070f64d59ad8c1d0870a49
rosipeguero <peg.rous@gmail.com>
2026-03-17 22:20:35 -0600

    Update ClaseEjemploParaGit.java
    Actualización de línea a una clase java

--- src/curso_selenium/ClaseEjemploParaGit.java
+++ src/curso_selenium/ClaseEjemploParaGit.java
@@ -4,6 +4,8 @@ public class ClaseEjemploParaGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		//Esto es un ejemplo más para conocer como funciona git ambiente gráfico con eclipse.
 
 	}
 
